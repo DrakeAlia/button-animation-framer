54e2431407fd6860a18f35a5b1426e228fbb574e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-07-29 17:03:50 -0700

    created button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
-import Image from "next/image";
+"use client";
+
+import { stagger, useAnimate } from "framer-motion";
 
 export default function Home() {
+  const [scope, animate] = useAnimate();
+
+  const onButtonClick = () => {
+    animate([[".letter", { y: -32 }, { duration: 0.2, delay: stagger(0.05) }]]);
+  };
+
   return (
-    <div>
-      
+    <div ref={scope}>
+      <button
+        onClick={onButtonClick}
+        className="rounded-full text-2xl border-2 border-blue-600 px-6 py-2 text-blue-600 hover:bg-blue-100 transition-colors"
+      >
+        <span className="sr-only">Motion</span>
+        <span
+          className="block h-8 overflow-hidden"
+          aria-hidden
+        >
+          {["M", "o", "t", "i", "o", "n"].map((letter, index) => (
+            <span
+              data-letter={letter}
+              className="letter relative inline-block h-8 after:absolute after:left-0
+              after:top-full after:h-8 after:content-[attr(data-letter)] leading-8"
+              key={`${letter}-${index}`}
+            >
+              {letter}
+            </span>
+          ))}
+        </span>
+      </button>
     </div>
   );
 }
