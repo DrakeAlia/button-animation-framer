2a5ddc0d95d6dc57e0e76f911514a767eaf4aa33
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-07-31 15:36:53 -0700

    finished creating the framer motion button with animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,74 @@
 "use client";
 
-import { stagger, useAnimate } from "framer-motion";
+import { animate, stagger, useAnimate } from "framer-motion";
+
+const randomNumberBetween = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+type AnimationSequence = Parameters<typeof animate>[0];
 
 export default function Home() {
   const [scope, animate] = useAnimate();
 
   const onButtonClick = () => {
-    animate([[".letter", { y: -32 }, { duration: 0.2, delay: stagger(0.05) }]]);
+    const sparkles = Array.from({ length: 20 });
+    const sparklesAnimation: AnimationSequence = sparkles.map((_, index) => [
+      `.sparkle-${index}`,
+      {
+        x: randomNumberBetween(-100, 100),
+        y: randomNumberBetween(-100, 100),
+        scale: randomNumberBetween(1.5, 2.5),
+        opacity: 1,
+      },
+      {
+        duration: 0.4,
+        at: "<",
+      },
+    ]);
+
+    const sparklesFadeOut: AnimationSequence = sparkles.map((_, index) => [
+      `.sparkle-${index}`,
+      {
+        opacity: 0,
+        scale: 0,
+      },
+      {
+        duration: 0.3,
+        at: "<",
+      },
+    ]);
+
+    const sparklesReset: AnimationSequence = sparkles.map((_, index) => [
+      `sparkle-${index}`,
+      {
+        x: 0,
+        y: 0,
+      },
+      {
+        duration: 0.000001,
+      },
+    ]);
+
+    animate([
+      ...sparklesReset,
+      [".letter", { y: -32 }, { duration: 0.2, delay: stagger(0.05) }],
+      ["button", { scale: 0.8 }, { duration: 0.1, at: "<" }],
+      ["button", { scale: 1 }, { duration: 0.1 }],
+      ...sparklesAnimation,
+      [".letter", { y: 0 }, { duration: 0.000001 }],
+      ...sparklesFadeOut,
+    ]);
   };
 
   return (
     <div ref={scope}>
       <button
         onClick={onButtonClick}
-        className="rounded-full text-2xl border-2 border-blue-600 px-6 py-2 text-blue-600 hover:bg-blue-100 transition-colors"
+        className="rounded-full text-2xl border-2 border-blue-600 px-6 py-2 text-blue-600 hover:bg-blue-100 transition-colors relative"
       >
         <span className="sr-only">Motion</span>
-        <span
-          className="block h-8 overflow-hidden"
-          aria-hidden
-        >
+        <span className="block h-8 overflow-hidden" aria-hidden>
           {["M", "o", "t", "i", "o", "n"].map((letter, index) => (
             <span
               data-letter={letter}
@@ -29,6 +78,25 @@ export default function Home() {
             >
               {letter}
             </span>
+          ))}
+        </span>
+        <span
+          aria-hidden
+          className="absolute inset-0 block pointer-events-none -z-10"
+        >
+          {Array.from({ length: 20 }).map((_, index) => (
+            <svg
+              className={`absolute opacity-0 left-1/2 top-1/2 sparkle-${index}`}
+              key={index}
+              viewBox="0 0 122 117"
+              width="10"
+              height="10"
+            >
+              <path
+                className="fill-blue-600"
+                d="M64.39,2,80.11,38.76,120,42.33a3.2,3.2,0,0,1,1.83,5.59h0L91.64,74.25l8.92,39a3.2,3.2,0,0,1-4.87,3.4L61.44,96.19,27.09,116.73a3.2,3.2,0,0,1-4.76-3.46h0l8.92-39L1.09,47.92A3.2,3.2,0,0,1,3,42.32l39.74-3.56L58.49,2a3.2,3.2,0,0,1,5.9,0Z"
+              />
+            </svg>
           ))}
         </span>
       </button>
